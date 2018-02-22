fdea4668b9ade70eb559252f8d0e776c0f686fac
Jarry <runfengai@163.com>
2018-02-22 15:48:43 +0800

    delete unused package

--- app/src/main/java/com/jarry/wechat/ui/BaseActivity.java
+++ app/src/main/java/com/jarry/wechat/ui/BaseActivity.java
@@ -2,22 +2,7 @@ package com.jarry.wechat.ui;
 
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.jarry.wechat.R;
-
-import java.util.List;
-
-import butterknife.ButterKnife;
 
 public abstract class BaseActivity extends AppCompatActivity {
     @LayoutRes
